84f00f1d2cd768ad570ed31779d7e460c1e2a7ee
Lera <lera.tananushko.jazzteam@gmail.com>
2021-03-21 12:29:09 +0300

    negative tests (login, password) for login page

--- test_login_page.py
+++ test_login_page.py
@@ -12,15 +12,36 @@ class TestLogin:
 
 
     invalid_acc = [r'  SOA424824', r'SOA424824   ', 'SOA4248 2 4']
+    invalid_login = [r'  letmein', r'letmein   ', 'l et me in']
+    invalid_password = [r'  letmein', r'letmein   ', 'l et me in']
 
     @pytest.mark.negative
     @pytest.mark.xfail
     @pytest.mark.parametrize('inv_acc', invalid_acc)
-    def test_invalid_login(self, browser, inv_acc):
+    def test_invalid_account(self, browser, inv_acc):
         link = 'http://18.213.119.207/salesorder/pages/login.aspx'
         page = LoginPage(browser, link)
         page.open()
         page.login(inv_acc, 'letmein', 'letmein')
+
+    @pytest.mark.negative
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('inv_login', invalid_login)
+    def test_invalid_login(self, browser, inv_login):
+        link = 'http://18.213.119.207/salesorder/pages/login.aspx'
+        page = LoginPage(browser, link)
+        page.open()
+        page.login('SOA424824', inv_login, 'letmein')
+
+    @pytest.mark.negative
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('inv_pass', invalid_password)
+    def test_invalid_login(self, browser, inv_pass):
+        link = 'http://18.213.119.207/salesorder/pages/login.aspx'
+        page = LoginPage(browser, link)
+        page.open()
+        page.login('SOA424824', 'letmein', inv_pass)
+
 
 
 
