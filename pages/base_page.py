5a317ac1515c17bda63cafeeec13d5874630c666
UserPes <abissineia7@gmail.com>
2021-04-01 10:08:42 +0300

    added list all so test, forgot base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,19 +2,13 @@ from selenium.common.exceptions import NoSuchElementException
 from .locators import MainPageLocators as MPL
 
 
+
 class BasePage:
 
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
-
-    def go_to_subsection(self, section, subsection):
-        self.browser.find_element(*MPL.COLLAPSE_BTN).click()
-        self.browser.find_element(section).click()
-        self.browser.find_element(subsection).click()
-
-
 
 
     def is_element_present(self, how, what):
@@ -30,9 +24,5 @@ class BasePage:
         self.browser.get(self.url)
 
 
-    def valid_login(self):
-        link = 'http://18.213.119.207/salesorder/pages/login.aspx'
-        page = LoginPage(browser, link)
-        page.open()
-        page.login('SOA424824', 'letmein', 'letmein')
+
 
