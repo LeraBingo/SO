bd1dde4e91cac0b6a3b1d6112d5aa69af0bd35be
UserPes <abissineia7@gmail.com>
2021-04-08 11:11:01 +0300

    reforming sales_order_page

--- pages/sales_orders_page.py
+++ pages/sales_orders_page.py
@@ -3,27 +3,33 @@ from .base_page import BasePage
 from .locators import MainPageLocators as MPL
 from selenium.webdriver.support.ui import Select
 
+
 class SalesOrder(BasePage):
 
-    def go_to_all_so(self):
-        frame = self.browser.find_element_by_css_selector('#tree')
-        self.browser.switch_to.frame(frame)
-        self.browser.find_element(*MPL.SALES).click()
-        self.browser.find_element(*MPL.SO).click()
-        self.browser.find_element(*MPL.SALES).click()
-        self.browser.switch_to_default_content()
-        frame = self.browser.find_element_by_css_selector('#workarea')
-        self.browser.switch_to.frame(frame)
-        self.browser.find_element(*MPL.LIST_ALL).click()
+    # adds an item (using item search). Now it adds one specified item
 
+    def add_item(self):
+        self.browser.find_element(*MPL.FIND_ITEM).click()
+        self.browser.find_element(*MPL.SEARCH_ITEM_IN_ITEM_SELECTOR).send_keys('0001')
+        self.browser.find_element(*MPL.ADD_ITEM_IN_ITEM_SELECTOR).click()
+        self.browser.find_element(*MPL.APPLY_IN_ITEM_SEARCH).click()
 
+    # adds several(number_of_items) using item search. The number of it items can be specified in the test-function
 
-    def search_so_by_ref_and_view(self):
-        self.browser.find_element(*MPL.SEARCH_BY_REF).send_keys('SO10897')
-        self.browser.find_element(*MPL.SEARCH_TRNXS).click()
-        assert self.is_element_present(*MPL.MAGNIFYING_GLASS_VIEW_SO), "No orders with the ref#"
-        self.browser.find_element(*MPL.MAGNIFYING_GLASS_VIEW_SO).click()
+    def add_several_random_items(self, number_of_items):
+        self.browser.find_element(*MPL.FIND_ITEM).click()
+        self.browser.find_element(*MPL.SEARCH_ITEM_IN_ITEM_SELECTOR).send_keys('*')
+        self.browser.find_element(*MPL.SEARCH_BTN_IN_ITEM_SELECTOR).click()
+        for item in range(1, number_of_items):
+            self.browser.find_element(By.XPATH, f"//tr[{item}]/td/img[@alt='add']").click()
+        self.browser.find_element(*MPL.APPLY_IN_ITEM_SEARCH).click()
 
+    # applies the entered PL
+
+    def apply_pl(self):
+        Select(self.browser.find_element(*MPL.APL_DROPDOWN)).select_by_visible_text('cats')
+
+    # creates a SO with 1 item and a PL applied, checks order has been created
 
     def create_so(self):
         self.browser.find_element(*MPL.ADD_TRNX).click()
@@ -36,6 +42,8 @@ class SalesOrder(BasePage):
         header_text = self.browser.find_element(*MPL.HEADER_TX).text
         assert header_text.startswith('View'), "Order has not been created"
 
+    # creates a SO with several items(their number can be specified), checks the order has been created
+
     def create_so_with_several_items(self, number_of_items):
         self.browser.find_element(*MPL.ADD_TRNX).click()
         self.browser.find_element(*MPL.SEARCH_BY_REF).send_keys('C10008')
@@ -46,49 +54,44 @@ class SalesOrder(BasePage):
         header_text = self.browser.find_element(*MPL.HEADER_TX).text
         assert header_text.startswith('View'), "Order has not been created"
 
-
-    def add_item(self):
-        self.browser.find_element(*MPL.FIND_ITEM).click()
-        self.browser.find_element(*MPL.SEARCH_ITEM_IN_ITEM_SELECTOR).send_keys('0001')
-        self.browser.find_element(*MPL.ADD_ITEM_IN_ITEM_SELECTOR).click()
-        self.browser.find_element(*MPL.APPLY_IN_ITEM_SEARCH).click()
-
-    def add_several_random_items(self, number_of_items):
-        self.browser.find_element(*MPL.FIND_ITEM).click()
-        self.browser.find_element(*MPL.SEARCH_ITEM_IN_ITEM_SELECTOR).send_keys('*')
-        self.browser.find_element(*MPL.SEARCH_BTN_IN_ITEM_SELECTOR).click()
-        for item in range(1, number_of_items):
-            self.browser.find_element(By.XPATH, f"//tr[{item}]/td/img[@alt='add']").click()
-        self.browser.find_element(*MPL.APPLY_IN_ITEM_SEARCH).click()
-
-
-
-
-    def apply_pl(self):
-        Select(self.browser.find_element(*MPL.APL_DROPDOWN)).select_by_visible_text('cats')
+    # this is the basic: edit->save changes. Changes should be implemented separately
 
     def edit_so(self):
         self.browser.find_element(*MPL.EDIT_TRNX).click()
-        #here we do some stuff
+        # here we do some stuff
         self.browser.find_element(*MPL.SAVE_AFTER_EDIT).click()
 
+    # shows all SOs
+
+    def go_to_all_so(self):
+        frame = self.browser.find_element_by_css_selector('#tree')
+        self.browser.switch_to.frame(frame)
+        self.browser.find_element(*MPL.SALES).click()
+        self.browser.find_element(*MPL.SO).click()
+        self.browser.find_element(*MPL.SALES).click()
+        self.browser.switch_to_default_content()
+        frame = self.browser.find_element_by_css_selector('#workarea')
+        self.browser.switch_to.frame(frame)
+        self.browser.find_element(*MPL.LIST_ALL).click()
+
+    # saves PL, tax and price level before changing and compares them with values after edit->save
 
     def remember_order_values(self):
-        order_general_values_before_edit = {'pl':'', 'tax':'', 'plevel':''}
+        order_general_values_before_edit = {'pl': '', 'tax': '', 'plevel': ''}
         order_general_values_after_edit = {'pl': '', 'tax': '', 'plevel': ''}
-        order_general_values_before_edit['pl'] =  self.browser.find_element(*MPL.PL_VALUE).text
+        order_general_values_before_edit['pl'] = self.browser.find_element(*MPL.PL_VALUE).text
         order_general_values_before_edit['tax'] = self.browser.find_element(*MPL.TAX_VALUE).text
-        order_general_values_before_edit['plevel']  = self.browser.find_element(*MPL.PLEVEL_VALUE).text
+        order_general_values_before_edit['plevel'] = self.browser.find_element(*MPL.PLEVEL_VALUE).text
         self.edit_so()
         order_general_values_after_edit['pl'] = self.browser.find_element(*MPL.PL_VALUE).text
         order_general_values_after_edit['tax'] = self.browser.find_element(*MPL.TAX_VALUE).text
         order_general_values_after_edit['plevel'] = self.browser.find_element(*MPL.PLEVEL_VALUE).text
         assert order_general_values_before_edit == order_general_values_after_edit, f'Actual data = {order_general_values_after_edit}, expected - {order_general_values_before_edit} '
 
+    # searches a so by ref# (indicated in send_keys for now) and opens in
 
-
-
-
-
-
-
+    def search_so_by_ref_and_view(self):
+        self.browser.find_element(*MPL.SEARCH_BY_REF).send_keys('SO10897')
+        self.browser.find_element(*MPL.SEARCH_TRNXS).click()
+        assert self.is_element_present(*MPL.MAGNIFYING_GLASS_VIEW_SO), "No orders with the ref#"
+        self.browser.find_element(*MPL.MAGNIFYING_GLASS_VIEW_SO).click()
