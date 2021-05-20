3fb53a9a5cf4905a3aee1145e1dd8ff78ae3f14c
UserPes <abissineia7@gmail.com>
2021-05-20 12:28:31 +0300

    +create so from cus with def pl

--- pages/customer_page.py
+++ pages/customer_page.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import Select
 class Customers(BasePage):
 
     def add_cus_pl(self, pl):
+        self.browser.find_element(*CPL.CUS_PROFILE_TAB).click()
         Select(self.browser.find_element(*CPL.CUS_PL)).select_by_visible_text(pl)
         return pl
 
@@ -16,11 +17,15 @@ class Customers(BasePage):
         self.browser.find_element(*CPL.CUS_NAME).send_keys(name)
         Select(self.browser.find_element(*CPL.CUS_CURRENCY)).select_by_visible_text(currency)
 
+    # Actions -> Create New SO
     def create_so_from_cus(self):
         self.browser.find_element(*CPL.CUS_ACTIONS).click()
         self.browser.find_element(*CPL.CUS_NEW_SO_OPTION).click()
 
-
+    def edit_cus(self):
+        self.browser.find_element(*CPL.CUS_EDIT).click()
+        header_text = self.browser.find_element(*CPL.CUS_HEADER_TX).text
+        assert header_text.startswith('Edit'), f'The item has not been edited'
 
     def list_all_customers(self):
         frame = self.browser.find_element_by_css_selector('#tree')
@@ -34,8 +39,13 @@ class Customers(BasePage):
         self.browser.switch_to.frame(frame)
         self.browser.find_element(*MPL.LIST_ALL).click()
 
-    def save_new_cus(self):
+    def save_changes(self):
         self.browser.find_element(*CPL.CUS_SAVE_BTN).click()
+        header_text = self.browser.find_element(*CPL.CUS_HEADER_TX).text
+        assert header_text.startswith('View'), f'View Customer'
+
+    def save_new_cus(self):
+        self.browser.find_element(*CPL.CUS_SAVE_NEW_CUS_BTN).click()
         header_text = self.browser.find_element(*CPL.CUS_HEADER_TX).text
         assert header_text.startswith('View'), f'View Customer'
 
--- test_customer_page.py
+++ test_customer_page.py
@@ -1,5 +1,8 @@
 from pages.customer_page import Customers as CU
 from pages.login_page import LoginPage
+from pages.items_page import Items
+from pages.sales_orders_page import SalesOrder as SO
+from pages.locators import MainPageLocators as MPL
 import time
 
 class TestCustomers:
@@ -13,6 +16,50 @@ class TestCustomers:
         cus.list_all_customers()
         cus.create_cus('testC', 'USD')
         cus.save_new_cus()
+
+
+    def test_create_so_from_cus(self,browser):
+        cus_name = '3M COMPANY'
+        num_of_items = 2
+        items = ['0001', '0002']
+
+        link = 'http://18.213.119.207/salesorder/pages/login.aspx'
+        page = LoginPage(browser, link)
+        page.open()
+        page.login('SOA424824', 'letmein', 'letmein')
+        cus = CU(browser, link)
+        cus.list_all_customers()
+        cus.search_cus_by_ref(cus_name)
+        cus.view_cus()
+        cus.create_so_from_cus()
+        so= SO(browser, link)
+        so.add_several_items_by_ref(num_of_items, *items)
+        so.save_so()
+
+    def test_create_so_from_cus_with_cus_pl(self, browser):
+        cus_name = '3M COMPANY'
+        pl_name = '5110'
+        num_of_items = 2
+        items = ['0001', '0002']
+
+        link = 'http://18.213.119.207/salesorder/pages/login.aspx'
+        page = LoginPage(browser, link)
+        page.open()
+        page.login('SOA424824', 'letmein', 'letmein')
+        cus = CU(browser, link)
+        cus.list_all_customers()
+        cus.search_cus_by_ref(cus_name)
+        cus.view_cus()
+        cus.edit_cus()
+        cus.add_cus_pl(pl_name)
+        cus.save_changes()
+        cus.create_so_from_cus()
+        so = SO(browser, link)
+        so.add_several_items_by_ref(num_of_items, *items)
+        so.save_so()
+        actual_pl = browser.find_element(*MPL.PL_VALUE).text
+        assert pl_name == actual_pl, f'expected pl - {pl_name}, actual pl - {actual_pl}'
+
 
     def test_go_to_customers_list(self, browser):
         link = 'http://18.213.119.207/salesorder/pages/login.aspx'
